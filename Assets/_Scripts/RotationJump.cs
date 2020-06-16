6079844ca9f3e13fec87f59216311e23e3a45bac
EagleSixty6 <daniel.zielasko@rwth-aachen.de>
2020-06-16 15:58:57 +0200

    ADD sound feedback for jump rotation

--- Assets/_Scripts/RotationJump.cs
+++ Assets/_Scripts/RotationJump.cs
@@ -5,12 +5,13 @@ using Valve.VR;
 
 public class RotationJump : MonoBehaviour
 {
+    public bool enableAudioFeedback;
     public float translationSpeedFactor;
 
     public float angleThreshold;
     public bool useRollInstead;
 
-    public float minJumpAngle;
+    public float defaultJumpSize;
     public bool enableIncreasingAngle;
     public float jumpDoulblingAngle;
 
@@ -49,14 +50,19 @@ public class RotationJump : MonoBehaviour
             }
 
             if (rotation < 180 && rotation > angleThreshold)
-            {
+            { 
+                if(enableAudioFeedback)
+                {
+                    GetComponent<AudioSource>().Play();
+                }
+                
                 float overshootInDegree = rotation - angleThreshold;
                 float multiplyer = 1;
                 if(enableIncreasingAngle)
                 {
                     multiplyer += overshootInDegree / jumpDoulblingAngle;
                 }
-                this.transform.Rotate(Vector3.up, minJumpAngle * multiplyer);
+                this.transform.Rotate(Vector3.up, defaultJumpSize * multiplyer);
 
                 multiplyer = 1;
                 if (enableDecreasingTime)
@@ -68,13 +74,18 @@ public class RotationJump : MonoBehaviour
             }
             else if(rotation > 180 && rotation < 360 - angleThreshold)
             {
+                if (enableAudioFeedback)
+                {
+                    GetComponent<AudioSource>().Play();
+                }
+
                 float overshootInDegree = 360 - angleThreshold - rotation;
                 float multiplyer = 1;
                 if (enableIncreasingAngle)
                 {
                     multiplyer += overshootInDegree / jumpDoulblingAngle;
                 }
-                this.transform.Rotate(Vector3.up, -minJumpAngle * multiplyer);
+                this.transform.Rotate(Vector3.up, -defaultJumpSize * multiplyer);
 
                 multiplyer = 1;
                 if (enableDecreasingTime)
