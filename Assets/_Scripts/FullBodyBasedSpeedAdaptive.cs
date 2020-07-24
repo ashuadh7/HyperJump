968084bbd43898dcc6a819566c0f4d9378868d93
EagleSixty6 <daniel.zielasko@rwth-aachen.de>
2020-07-24 16:17:55 +0200

    ADD: interface switch between straving and roation

--- Assets/_Scripts/FullBodyBasedSpeedAdaptive.cs
+++ Assets/_Scripts/FullBodyBasedSpeedAdaptive.cs
@@ -10,6 +10,10 @@ public class FullBodyBasedSpeedAdaptive : MonoBehaviour
     [Tooltip("Changes the speed of forward and backward translation.")]
     public float _translationSpeedFactor;
 
+    [Tooltip("Gives the point in percent of the maximum speed where strafing is replaced by rotating.")]
+    [Range(0f, 1f)]
+    public float _velocityThesholdForInterfaceSwitch;
+
     public float _continuesRotationSpeedFactor;
     #endregion
 
@@ -24,18 +28,26 @@ public class FullBodyBasedSpeedAdaptive : MonoBehaviour
     {
         this.transform.position += this.transform.forward * GetComponent<LocomotionControl>().GetLeaningAxis().y * Time.deltaTime * _translationSpeedFactor;
 
-        // when slow strafe here leaning
+        // when slow enough leaning controlles strafing
+        if (GetComponent<LocomotionControl>().GetLeaningAxis().y < _velocityThesholdForInterfaceSwitch)
+        {
+            this.transform.position += this.transform.right * GetComponent<LocomotionControl>().GetLeaningAxis().x * Time.deltaTime * _translationSpeedFactor;
+        }
+
     }
 
     private void Rotate()
     {
-        float rotationSpeedFactor = _continuesRotationSpeedFactor * (1.0f - GetComponent<LocomotionControl>().GetLeaningAxis().y);
+        // when fast enough leaning controlles rotation
+        if (GetComponent<LocomotionControl>().GetLeaningAxis().y >= _velocityThesholdForInterfaceSwitch)
+        {
+            float rotationSpeedFactor = _continuesRotationSpeedFactor * (1.0f - GetComponent<LocomotionControl>().GetLeaningAxis().y);
 
-        // TODO when fast: roll and leaning rotates, when slow: only roll / yaw
-        this.transform.RotateAround(
-            GameObject.Find("Camera").transform.position, 
-            Vector3.up,
-            rotationSpeedFactor * Time.deltaTime * GetComponent<LocomotionControl>().GetLeaningAxis().x
-        );
+            this.transform.RotateAround(
+                GameObject.Find("Camera").transform.position, 
+                Vector3.up,
+                rotationSpeedFactor * Time.deltaTime * GetComponent<LocomotionControl>().GetLeaningAxis().x
+            );
+        }
     }
 }
